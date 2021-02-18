a8a86df6e2478c417f63994a2167be3da9c4dccd
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2021-02-17 23:26:26 -0600

    initial commit to github

---
+++ character_data.sql
@@ -0,0 +1,56 @@
+/* fortune500 table fields are 
+
+rank 
+title 
+name 
+ticker
+url
+hq 
+sector
+industry
+employees
+revenue
+revenues_change
+profit
+profits_change
+assets
+equity
+
+*/
+
+/* company table fields are 
+
+id
+exchange
+ticker 
+name
+parent_id
+
+*/
+
+/* stackoverflow table fields are 
+
+id
+tag
+date
+question_count
+question_pct
+unanswered_count
+unanswered_pct
+
+*/
+
+/* tag_type table fields are 
+
+id
+tag
+type
+
+*/
+
+/* tag_company table fields are 
+
+tag
+company_id
+
+*/
